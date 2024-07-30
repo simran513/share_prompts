faff88ec1a6552a1d118d347eb4889777712304d
simran513 <khandualsimran@gmail.com>
2024-07-31 01:17:15 +0530

    adding suspense tag

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Form from '@components/Form'
+import { Suspense } from 'react'
+
 
 const EditPrompt = () => {
     const router = useRouter()
@@ -15,25 +17,25 @@ const EditPrompt = () => {
     })
 
     useEffect(() => {
-      const getPromptDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptId}`)
-        const data = response.json()
-        setPost({
-            prompt: data.prompt,
-            tag: data.tag
-        })
-      }
-      if(promptId) getPromptDetails()
+        const getPromptDetails = async () => {
+            const response = await fetch(`/api/prompt/${promptId}`)
+            const data = response.json()
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag
+            })
+        }
+        if (promptId) getPromptDetails()
     }, [promptId])
-    
+
 
     const updatePrompt = async (e) => {
         e.preventDefault()
         setSubmitting(true)
 
-        if(!promptId) return alert('Prompt ID not found')
+        if (!promptId) return alert('Prompt ID not found')
 
-        try{
+        try {
             const response = await fetch(`/api/prompt/${promptId}`, {
                 method: 'PATCH',
                 body: JSON.stringify({
@@ -41,24 +43,26 @@ const EditPrompt = () => {
                     tag: post.tag
                 })
             })
-            if(response.ok){
+            if (response.ok) {
                 router.push('/')
             }
-        }catch(error){
+        } catch (error) {
             console.log(error)
-        }finally{
+        } finally {
             setSubmitting(false)
         }
     }
-  return (
-    <Form 
-        type='Edit'
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-    />
-  )
+    return (
+        <Suspense>
+            <Form
+                type='Edit'
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
+    )
 }
 
 export default EditPrompt
